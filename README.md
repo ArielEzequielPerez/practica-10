621cb5a2edf98359577e575bfb40c9d3385cd172
ArielEzequielPerez <ariperez@frba.utn.edu.ar>
2021-10-10 19:35:41 -0300

    falta agregar mas estilos

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # practica-10
  
-## 1.Apretando
-un botón, cambiar color de fondo de otro elemento.
+## 1.Punto 1
+Apretando un botón, cambiar color de fondo de otro elemento.
+
+## 2.Punto 2
+Apretando un botón, mostrar u ocultar una sección de la página.
 
 
-## 2.Apretando
-un botón, mostrar u ocultar una sección de la página.
+## 3.Punto 3
+Apretando un botón, agregar un elemento HTML que no existía en el .html original.
 
 
-## 3.Apretando
-un botón, agregar un elemento HTML que no existía en el .html
-original.
-
-
-## 4.Dado
-10 botones con texto del 0 al 9, mostrar en un input el valor del botón clickeado y cambiar color de fondo. Cuando se
+## 4. Punto 4
+Dado 10 botones con texto del 0 al 9, mostrar en un input el valor del botón clickeado y cambiar color de fondo. Cuando se
 vuelva a clickear, volver el color del fondo original y
 borrar el numero del input.
---
+++ js/aplicacion.js
@@ -0,0 +1,120 @@
+let bandera = true
+
+function cambiarColor(){
+    if(bandera){
+        $('h1').css('color', 'red');
+        bandera =false;
+    }else{
+        $('h1').css('color', 'black');
+        bandera =true;
+    }
+}
+function ocultarTexto(){
+    if(bandera){
+        $('p').hide();
+        bandera =false;
+    }else{
+        $('p').show();
+        bandera =true;
+    }
+}
+
+function agregarElemento(){
+    let texto = $("#agregarTexto").val();
+    $("#textoNuevo").html(`<p class ="letraNegrita"> ${texto} </p>`);
+}
+
+
+$(document).ready(function(){
+    $('button').addClass('btn').addClass('btn-primary');
+})
+const agregarBotones = () =>{
+    for(let indice = 0; indice < 10; indice++){
+        $('#button').append(`<button>${indice}</button>`)
+    }
+}
+
+$(document).ready(function(){
+    cambiarColorDeFondo('boton0' , 0);
+    cambiarColorDeFondo('boton1' , 1);
+    cambiarColorDeFondo('boton2' , 2);
+    cambiarColorDeFondo('boton3' , 3);
+    cambiarColorDeFondo('boton4' , 4);
+    cambiarColorDeFondo('boton5' , 5);
+    cambiarColorDeFondo('boton6' , 6);
+    cambiarColorDeFondo('boton7' , 7);
+    cambiarColorDeFondo('boton8' , 8);
+    cambiarColorDeFondo('boton9' , 9);
+
+
+})
+let cadena = "";
+function cambiarColorDeFondo(control, numero){
+    if(control === "boton0"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput('violet');
+        })
+    }
+    if(control === "boton1"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("aqua");
+        })
+    }
+    if(control === "boton2"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("red");
+        })
+    }
+    if(control === "boton3"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("aqua");
+        })
+    }
+    if(control === "boton4"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("blue");
+        })
+    }
+    if(control === "boton5"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("green");
+        })
+    }
+    if(control === "boton6"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("grey");
+        })
+    }
+    if(control === "boton7"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("blueviolet");
+        })
+    }
+    if(control === "boton8"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("chocolate");
+        })
+    }
+    if(control === "boton9"){
+        $('#'+control).click(function(){
+            mostrar(numero);
+            cambiarColorInput("fuchsia");
+        })
+    }
+}
+
+function mostrar(unNumero){
+    $('#mostrarNumero').val(unNumero);
+}
+function cambiarColorInput(color){
+    $('#mostrarNumero').css('background-color', color);
+}
